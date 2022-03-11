2512e1edc2f98344591d979f9fa7546aa2dcfa99
Levon <levonstepanyan.ait@gmail.com>
2022-03-11 15:01:16 +0400

    refactor(date picker) : few parts of code go to register page

--- src/components/mainPage/AboutPart/components/AboutFormPages/registerPage/RegisterPageComponents/DatePickerContainer.jsx
+++ src/components/mainPage/AboutPart/components/AboutFormPages/registerPage/RegisterPageComponents/DatePickerContainer.jsx
@@ -1,55 +1,82 @@
 // React
-import React, {useState} from "react"
+import React from "react";
 // components
 import DatePickerComponent from "../../../../../../../constant/DatePicker";
 
-function DatePickerContainer(){
+function DatePickerContainer({
+  selectDay,
+  selectMonth,
+  selectYear,
+  selectedMonth,
+  selectedYear,
+}) {
+  //  Months
+  const months = [
+    " ",
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
 
-//  Months
-    const months = [" " , "January","February","March","April","May","June","July",
-    "August","September","October","November","December"];
+  //  Start Options
+  const newDate = new Date();
+  let startYear = 1902;
+  let currentYear = newDate.getFullYear();
+  let lastDayInMonth = new Date(
+    selectedYear,
+    months.indexOf(selectedMonth),
+    0
+  ).getDate();
 
-//  useState for date
-    const [selectedYear, selectYear] = useState("");
-    const [selectedMonth, selectMonth] = useState("");
-    const [selectedDay, selectDay] = useState("");
-
-//  Start Options
-    const newDate = new Date();
-    let startYear = 1902;
-    let currentYear = newDate.getFullYear();
-    let lastDayInMonth = new Date(selectedYear, months.indexOf(selectedMonth), 0).getDate();
-    
-
-//  Years
-    const yearsForChoose = [' '];
-    for (let i = currentYear; i >= startYear - 1; i--) {
-        if (currentYear >= startYear) {
-        yearsForChoose.push(currentYear);
-        }
-        currentYear -= 1;
+  //  Years
+  const yearsForChoose = [" "];
+  for (let i = currentYear; i >= startYear - 1; i--) {
+    if (currentYear >= startYear) {
+      yearsForChoose.push(currentYear);
     }
+    currentYear -= 1;
+  }
 
-//  Days
-    const daysForChoose = [' '];
-    if(selectedMonth){
-        for(let i = 1 ; i <= lastDayInMonth ; i++){
-            daysForChoose.push(i)
-        }
+  //  Days
+  const daysForChoose = [" "];
+  if (selectedMonth) {
+    for (let i = 1; i <= lastDayInMonth; i++) {
+      daysForChoose.push(i);
     }
-    else{
-        for(let i = 1 ; i <= 31 ; i++){
-            daysForChoose.push(i)
-        }
+  } else {
+    for (let i = 1; i <= 31; i++) {
+      daysForChoose.push(i);
     }
+  }
 
-    return (
-        <div className="datePickerContainer">
-            <DatePickerComponent data = {months} selectMonth = {selectMonth} selectType = "Month" />
-            <DatePickerComponent data = {daysForChoose} selectDay = {selectDay}  selectType = "Day" />
-            <DatePickerComponent data={yearsForChoose} selectYear={selectYear} selectType = "Year"/>
-        </div>
-    )
+  return (
+    <div className="datePickerContainer">
+      <DatePickerComponent
+        data={months}
+        selectMonth={selectMonth}
+        selectType="Month"
+      />
+      <DatePickerComponent
+        data={daysForChoose}
+        selectDay={selectDay}
+        selectType="Day"
+      />
+      <DatePickerComponent
+        data={yearsForChoose}
+        selectYear={selectYear}
+        selectType="Year"
+      />
+    </div>
+  );
 }
 
-export default DatePickerContainer
+export default DatePickerContainer;
