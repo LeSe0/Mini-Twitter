e59d70e437877fde21ae8f252ab421e261db675c
Levon <levonstepanyan.ait@gmail.com>
2022-03-11 14:58:25 +0400

    feat(text field container) : contain all register page text fields

---
+++ src/components/mainPage/AboutPart/components/AboutFormPages/registerPage/RegisterPageComponents/TextFieldContainer.jsx
@@ -0,0 +1,71 @@
+// React
+import React, { useState } from "react";
+// components
+import CustomTextField from "../../../../../../../constant/CustomTextField";
+// MUI
+import { Box } from "@mui/material";
+import { Typography } from "@mui/material";
+
+function TextFieldContainer({ setEmail, setNameValue, addError , setPhone, email , phone , name }) {
+  const [registerBy, setRegisterBy] = useState("phone");
+  const [errorForPhone , setErrorForPhone] = useState(false);
+  const [errorForEmail , setErrorForEmail] = useState(false)
+
+  let toggleButton = registerBy == "phone" ? "email" : "phone";
+
+  const activeRegisterBy = {
+    phone: (
+      <CustomTextField
+        labelText={"Phone"}
+        errorText={"Please enter a valid phone number."}
+        setPhone = {setPhone}
+        addError = {addError}
+        phone = {phone}
+        setErrorForPhone = {setErrorForPhone}
+        errorForPhone = {errorForPhone}
+      />
+    ),
+    email: (
+      <CustomTextField
+        labelText={"Email"}
+        errorText={"Please enter a valid email."}
+        setEmail = {setEmail}
+        addError = {addError}
+        email = {email}
+        setErrorForEmail = {setErrorForEmail}
+        errorForEmail = {errorForEmail}
+      />
+    ),
+  };
+
+  return (
+    <Box className="textFieldContainer">
+      <CustomTextField
+        labelText={"Name"}
+        errorText={"What’s your name?"}
+        setNameValue={setNameValue}
+        addError = {addError}
+        name = {name}
+      />
+      {activeRegisterBy[registerBy]}
+      <Typography
+        component="span"
+        sx={{
+          color: "#1D9BF0",
+          cursor: "pointer",
+        }}
+        onClick={() => {
+          setRegisterBy(toggleButton);
+          setPhone('')
+          setEmail('')
+          setErrorForEmail(false);
+          setErrorForPhone(false);
+        }}
+      >
+        Use {toggleButton} instead
+      </Typography>
+    </Box>
+  );
+}
+
+export default TextFieldContainer;
