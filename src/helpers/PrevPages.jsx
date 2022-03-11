7563d0dc5b22e24340bef5cfe678032506461dd3
Levon <levonstepanyan.ait@gmail.com>
2022-03-11 14:56:13 +0400

    feat(prev pages) : do this button more usable

--- src/helpers/PrevPages.jsx
+++ src/helpers/PrevPages.jsx
@@ -1,17 +1,25 @@
 // React
 import React from "react";
+// MUI
+import { Box } from "@mui/system";
+import CloseIcon from "@mui/icons-material/Close";
 
-export default function PrevPages({data , setActivePage}){
-    return (
-        <div>
-            {data.map((el , i) =>{
-                return (
-                    <button key = {el.name + i} onClick = {(e) =>{
-                        e.preventDefault();
-                        setActivePage(el.prevPage)
-                    }}>{el.name}</button>
-                )
-            })}
-        </div>
-    )
+export default function PrevPages({ data, setActivePage, icon }) {
+  const switchIcons = {
+    close: (
+      <CloseIcon
+        sx={{
+          cursor: "pointer",
+        }}
+        key={data.key}
+        onClick={(e) => {
+          e.preventDefault();
+          setActivePage(data.prevPage);
+        }}
+      />
+    ),
+    prev: null,
+  };
+
+  return <Box>{switchIcons[icon]}</Box>;
 }
