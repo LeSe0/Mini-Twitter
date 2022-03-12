8082c234d0ba9d2b96286ad8faf56b8390dc1390
Levon <levonstepanyan.ait@gmail.com>
2022-03-12 16:12:45 +0400

    feat(register) : add last step and layout sign in button

--- src/components/mainPage/AboutPart/components/AboutFormPages/registerPage/RegisterPage.jsx
+++ src/components/mainPage/AboutPart/components/AboutFormPages/registerPage/RegisterPage.jsx
@@ -4,9 +4,10 @@ import { React, useState } from "react";
 import FirstStep from "./RegisterPageComponents/firstStep/FirstStep";
 import SecondStep from "./RegisterPageComponents/secondStep/SecondStep";
 import PrevPages from "../../../../../../helpers/PrevPages";
-import Steps from "../../../../../../helpers/Steps"
+import Steps from "../../../../../../helpers/Steps";
+import LastStep from "./RegisterPageComponents/lastStep/LastStep";
 // MUI
-import { Box, Grid } from "@mui/material";
+import { Box, Grid, Typography, Button } from "@mui/material";
 
 export default function RegisterPage({ setActivePage }) {
   const months = {
@@ -33,7 +34,7 @@ export default function RegisterPage({ setActivePage }) {
   const [error, addError] = useState(false);
   const [errorForPhone, setErrorForPhone] = useState(false);
   const [errorForEmail, setErrorForEmail] = useState(false);
-  const [step , setStep] = useState(1);
+  const [step, setStep] = useState(1);
 
   let dateFromPicker =
     selectedYear &&
@@ -46,53 +47,89 @@ export default function RegisterPage({ setActivePage }) {
     key: "firstRegPage",
   };
 
-
   const steps = {
-    step : step,
-    stepsCount : 3
-  }
+    step: step,
+    stepsCount: 3,
+  };
 
   const activeStep = {
-    1 :       <FirstStep
-    setActivePage={setActivePage}
-    name={name}
-    setNameValue={setNameValue}
-    email={email}
-    setEmail={setEmail}
-    phone={phone}
-    setPhone={setPhone}
-    selectedYear={selectedYear}
-    selectYear={selectYear}
-    selectedMonth={selectedMonth}
-    selectMonth={selectMonth}
-    selectedDay={selectedDay}
-    selectDay={selectDay}
-    error={error}
-    addError={addError}
-    errorForPhone={errorForPhone}
-    errorForEmail={errorForEmail}
-    setErrorForEmail={setErrorForEmail}
-    setErrorForPhone={setErrorForPhone}
-    setStep = {setStep}
-    step = {step}
-  />,
-  2 : <SecondStep/>
-  }
+    1: (
+      <FirstStep
+        setActivePage={setActivePage}
+        name={name}
+        setNameValue={setNameValue}
+        email={email}
+        setEmail={setEmail}
+        phone={phone}
+        setPhone={setPhone}
+        selectedYear={selectedYear}
+        selectYear={selectYear}
+        selectedMonth={selectedMonth}
+        selectMonth={selectMonth}
+        selectedDay={selectedDay}
+        selectDay={selectDay}
+        error={error}
+        addError={addError}
+        errorForPhone={errorForPhone}
+        errorForEmail={errorForEmail}
+        setErrorForEmail={setErrorForEmail}
+        setErrorForPhone={setErrorForPhone}
+        setStep={setStep}
+        step={step}
+      />
+    ),
+    2: <SecondStep setStep={setStep} step={step} />,
+    3: (
+      <LastStep
+        months={months}
+        selectedYear={selectedYear}
+        selectYear={selectYear}
+        selectedMonth={selectedMonth}
+        selectedDay = {selectedDay}
+        setStep={setStep}
+        step={step}
+        name = {name}
+        phone = {phone}
+        email = {email}
+      />
+    ),
+  };
 
   return (
     <Box className="mt-[12px]">
       <Grid container justifyContent={"space-between"}>
         <Grid item>
-          <PrevPages setActivePage={setActivePage} setStep = {setStep} data={step == 1 ? data : {
-            key : "goBackIcon",
-            prevPage : step - 1
-          }} icon={step == 1 ? "close" : "prev"} />
+          <PrevPages
+            setActivePage={setActivePage}
+            setStep={setStep}
+            data={
+              step == 1
+                ? data
+                : {
+                    key: "goBackIcon",
+                    prevPage: step - 1,
+                  }
+            }
+            icon={step == 1 ? "close" : "prev"}
+          />
         </Grid>
         <Grid item>
-          <Steps step = {steps.step} stepsCount = {steps.stepsCount}/>
+          <Steps step={steps.step} stepsCount={steps.stepsCount} />
         </Grid>
       </Grid>
       {activeStep[step]}
+
+      <Box>
+          <Typography sx = {{
+              color : "#0F1419 !important",
+              fontWeight : "700",
+              marginTop : "25px",
+              marginBottom : "10px"
+          }}>Already have an account?</Typography>
+        <Button type="button" variant="contained" className={secondButton} onClick = {() =>{
+            setActivePage("defaultPage")
+        }}>Sign in</Button>
+      </Box>
     </Box>
   );
 }
