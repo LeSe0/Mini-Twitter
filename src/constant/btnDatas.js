4e9111d1016ed0c438e86d8105af9a81b7b72d01
Levon <levonstepanyan.ait@gmail.com>
2022-03-15 17:06:56 +0400

    feat(button style) : add btn data js for minimal code

---
+++ src/constant/btnDatas.js
@@ -0,0 +1,11 @@
+export const nextButton = {
+    color: "white",
+    borderColor: "rgba(0,0,0,0)",
+    backgroundColor: "rgb(15, 20, 25)",
+    focusedColor: "white",
+    focusedBackground: "rgb(39, 44, 48)",
+    focusedBorder: "rgba(0,0,0,0)",
+    disabledColor: "white",
+    disabledBackground: "gray",
+    disabledBorder: "blue",
+}
