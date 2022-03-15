8371fe1ed522df186335e67a884c68355503291d
Levon <levonstepanyan.ait@gmail.com>
2022-03-15 17:37:01 +0400

    feat(forgot) : create forgot third steo

---
+++ src/components/mainPage/AboutPart/components/AboutFormPages/forgotPage/steps/ForgotThirdStep.jsx
@@ -0,0 +1,12 @@
+// React
+import React from "react"
+
+function ForgotThirdStep({setActivePage , setStep}){
+    return (
+        <form>
+            in this time cant find template of this page
+        </form>
+    )
+}
+
+export default ForgotThirdStep
